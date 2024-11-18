2bbe1232c4c276c44cb0c2b41f5b318f2117cca0
borkya1 <swapnil.santosh20@gmail.com>
2024-11-17 23:11:43 -0500

    Update login.js

--- login.js
+++ login.js
@@ -3,14 +3,17 @@
 console.log('Login script loaded');
 // Function to handle login
 function handleLogin(event) {
+    console.log('Login button clicked');
     event.preventDefault();
     const username = document.getElementById('username').value;
+    console.log('Username entered:', username);
     const password = document.getElementById('password').value;
+    console.log('Password entered:', password);
     
     if (username === 'admin' && password === 'password') {
+        console.log('Redirecting to content.html');
         window.location.href = window.location.origin + window.location.pathname.replace('index.html', 'content.html');
     } else {
         alert('Invalid username or password');
     }
-    console.log('Login script exited');
 }
